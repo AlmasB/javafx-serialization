f0a263d3ec164c66cefda04df60dab02b5c3a0dd
Almas Baim <almaslvl@gmail.com>
2024-01-01 20:18:03 +0000

    WIP

---
+++ src/main/kotlin/com/almasb/javafx/serialization/Entry.kt
@@ -0,0 +1,32 @@
+package com.almasb.javafx.serialization
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import javafx.scene.shape.Rectangle
+import javafx.scene.shape.StrokeType
+
+fun main() {
+    serial()
+    //test2()
+}
+
+private fun serial() {
+    val rect = Rectangle(200.0, 300.0, 1000.0, 150.0)
+
+    val str = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rect)
+
+    println(str)
+
+    val rect2 = mapper.readValue<Rectangle>(str)
+
+    println(rect2)
+}
+
+private fun test2() {
+    val clazz = StrokeType::class.java
+
+    val str = "INSIDE"
+
+    val value: StrokeType = clazz.enumConstants.find { it.name == str }!!
+
+    println(value)
+}
--- src/main/kotlin/com/almasb/javafx/serialization/std/Rectangle.kt
+++ src/main/kotlin/com/almasb/javafx/serialization/std/Rectangle.kt
@@ -1,5 +1,6 @@
 package com.almasb.javafx.serialization.std
 
+import com.almasb.javafx.serialization.mapper
 import com.almasb.javafx.serialization.toColor
 import com.almasb.javafx.serialization.toDouble
 import com.fasterxml.jackson.core.JsonGenerator
@@ -9,6 +10,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.fasterxml.jackson.module.kotlin.convertValue
 import javafx.scene.shape.Rectangle
 
 /**
@@ -20,11 +22,34 @@ class RectangleSerializer : StdSerializer<Rectangle>(Rectangle::class.java) {
     override fun serialize(value: Rectangle, gen: JsonGenerator, provider: SerializerProvider) {
         gen.writeStartObject()
 
-        gen.writeStringField("@class", value.javaClass.canonicalName)
-        gen.writeNumberField("width", value.width)
-        gen.writeNumberField("height", value.height)
+//        gen.writeStringField("@class", value.javaClass.canonicalName)
+//        gen.writeNumberField("width", value.width)
+//        gen.writeNumberField("height", value.height)
+//
+//        gen.writeObjectField("fill", value.fill)
 
-        gen.writeObjectField("fill", value.fill)
+        val methods = value.javaClass.methods
+        val methodNames = methods.map { it.name }
+
+        methods.filter { it.name.startsWith("get") }
+                .filter {
+                    val setterName = it.name.replaceFirstChar { 's' }
+
+                    setterName in methodNames && !setterName.startsWith("setOn")
+                }
+                .forEach {
+                    val result = it.invoke(value)
+
+                    val fieldName = it.name.removePrefix("get").replaceFirstChar { it.uppercaseChar() }
+
+                    if (result is Double) {
+                        gen.writeNumberField(fieldName, result)
+                    } else {
+                        gen.writeStringField(fieldName, "$result")
+                    }
+                }
+
+
 
         gen.writeEndObject()
     }
@@ -35,11 +60,48 @@ class RectangleDeserializer : StdDeserializer<Rectangle>(Rectangle::class.java) 
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Rectangle {
         val node = p.codec.readTree<JsonNode>(p)
 
-        val width = node.get("width").toDouble()
-        val height = node.get("height").toDouble()
+//        val width = node.get("width").toDouble()
+//        val height = node.get("height").toDouble()
+//
+//        val fill = node.get("fill").toColor()
 
-        val fill = node.get("fill").toColor()
+        val rect = Rectangle()
 
-        return Rectangle(width, height, fill)
+
+
+
+
+
+        val methods = rect.javaClass.methods
+        val methodNames = methods.map { it.name }
+
+        methods.filter { it.name.startsWith("set") }
+                .filter {
+                    val getterName = it.name.replaceFirstChar { 'g' }
+
+                    getterName in methodNames && !getterName.startsWith("getOn")
+                }
+                .forEach {
+                    val paramType = it.parameterTypes[0]
+
+                    val serializedValue = node.get(it.name.removePrefix("set").replaceFirstChar { it.uppercaseChar() })
+
+                    println("Calling ${it.name} with $serializedValue")
+
+
+
+                    if (paramType == Double::class.java) {
+
+                        // TODO: cast value to actual object
+                        it.invoke(rect, serializedValue.toDouble())
+                    } else if (paramType.isEnum) {
+
+                        val result = paramType.enumConstants.find { it.toString() == serializedValue.textValue() }
+
+                        it.invoke(rect, result)
+                    }
+                }
+
+        return rect
     }
 }
